10e1707237bcf1d13aa6a7c496a8fd2567d5a18a
Jonathan Porter <myctrring@gmail.com>
2015-09-01 12:46:13 -0600

    finished

--- game.js
+++ game.js
@@ -1,2 +1,50 @@
 var health = 100;
-function slap()
+
+updatePanel(health);
+
+function slap() {
+	showHide('showSlap');
+	health--;
+	if (health < 0)
+		health = 0;
+	updatePanel(health);
+}
+
+function punch() {
+	showHide('showPunch');
+	health = health - 5;
+	if (health < 0)
+		health = 0;
+	updatePanel(health);
+}
+
+function kick() {
+	showHide('showKick');
+	health = health - 10;
+	if (health < 0)
+		health = 0;
+	updatePanel(health);
+}
+
+function potion() {
+	health = health + 10;
+	if (health > 100)
+		health = 100;
+	updatePanel(health);
+}
+
+function showHide(id) {
+	document.getElementById(id).classList.remove("hide");
+	
+	var hide = function () {
+		document.getElementById(id).classList.add("hide");
+	}
+
+	setTimeout(hide, 500)
+
+}
+
+function updatePanel(health) {
+	document.querySelector('.panel-heading').innerHTML = health;
+}
+
