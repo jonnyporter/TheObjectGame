3d5b4f6dcc6af169bb56a339b6880551abdbd5da
Jonathan Porter <myctrring@gmail.com>
2015-09-03 20:33:14 -0600

    finished

--- game.js
+++ game.js
@@ -1,36 +1,71 @@
-var health = 100;
+var player = {
+	name: "ninja",
+	hits: 0,
+	health: 100,
+};
 
-updatePanel(health);
+player.name = prompt("Enter your name");
+if (player.name == null || player.name == '')
+	player.name = "ninja";
+
+naming(player.name);
+updatePanel(player.health, player.hits);
 
 function slap() {
 	showHide('showSlap');
-	health--;
-	if (health < 0)
-		health = 0;
-	updatePanel(health);
+	player.hits++;
+	updateHealth(player, -1);
+	updatePanel(player.health, player.hits);
 }
 
 function punch() {
 	showHide('showPunch');
-	health = health - 5;
-	if (health < 0)
-		health = 0;
-	updatePanel(health);
+	player.hits++;
+	updateHealth(player, -5);
+	updatePanel(player.health, player.hits);
 }
 
 function kick() {
 	showHide('showKick');
-	health = health - 10;
-	if (health < 0)
-		health = 0;
-	updatePanel(health);
+	player.hits++;
+	updateHealth(player, -10);
+	updatePanel(player.health, player.hits);
 }
 
 function potion() {
-	health = health + 10;
-	if (health > 100)
-		health = 100;
-	updatePanel(health);
+	updateHealth(player, 100);
+	updatePanel(player.health, player.hits);
+}
+
+function reset() {
+	updateHealth(player, 100);
+	player.hits = 0;
+	updatePanel(player.health, player.hits);
+
+}
+
+function updateHits(player, hitNum) {
+	if (player.hits < 0)
+		player.hits = 0;
+}
+
+function updateHealth(player, damage) {
+	player.health = player.health + damage;
+	if (player.health < 0)
+		player.health = 0;
+	if (player.health > 100)
+		player.health = 100;
+}
+
+function naming(playerName) {
+	document.getElementById('playerName').innerHTML = "name: " + player.name;
+}
+
+function updatePanel(health, hits) {
+	document.getElementById('hits').innerHTML = "hits: " + hits;
+	var progressBar = document.querySelector('.progress-bar');
+	progressBar.innerHTML = health;
+	progressBar.style.width = health + "%";
 }
 
 function showHide(id) {
@@ -43,8 +78,3 @@ function showHide(id) {
 	setTimeout(hide, 500)
 
 }
-
-function updatePanel(health) {
-	document.querySelector('.panel-heading').innerHTML = health;
-}
-
